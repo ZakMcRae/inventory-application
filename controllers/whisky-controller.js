d1d3bda9dc19b55b92cc8b6c185be382772ce3ea
Zak McRae <zakmcrae@gmail.com>
2021-09-09 15:23:09 -0400

    implemented cont/view for specific and all whisky

--- controllers/whisky-controller.js
+++ controllers/whisky-controller.js
@@ -1,18 +1,23 @@
 const Whisky = require("../models/whisky");
 const Category = require("../models/category");
 const Distillery = require("../models/distillery");
+const whisky = require("../models/whisky");
 
 exports.allWhisky = async function (req, res, next) {
-  // todo sort list for display purposes
+  // todo sort list for display purposes - possibly add filter options
   const whiskies = await Whisky.find({})
     .populate("category")
     .populate("distillery")
     .exec();
-  res.render("whisky", { title: "All Whiskies", whiskies: whiskies });
+  res.render("whisky-list", { title: "Whiskies", whiskies: whiskies });
 };
 
 exports.getWhisky = async function (req, res, next) {
-  res.send("not implemented");
+  const whisky = await Whisky.findById(req.params.id)
+    .populate("category")
+    .populate("distillery")
+    .exec();
+  res.render("whisky-detail", { title: whisky.name, whisky: whisky });
 };
 
 exports.createWhiskyGet = async function (req, res, next) {
